34c6d127d238c8a8d5b28a88694379df1ba492db
Stanislav Dimitrov <standimitroff@gmail.com>
2024-03-27 16:32:58 +0200

    Create `Sport` domain model

---
+++ CatalogueCore/CatalogueCore/Sports/Sport.swift
@@ -0,0 +1,13 @@
+//
+//  Sport.swift
+//  CatalogueCore
+//
+//  Created by Stanislav Dimitrov on 27.03.24.
+//
+
+import Foundation
+
+public struct Sport {
+  public let id: Int
+  public let name: String
+}
