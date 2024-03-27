47d280234017dafe9879220ab4c78553359e510a
Stanislav Dimitrov <standimitroff@gmail.com>
2024-03-27 16:09:55 +0200

    Create platform-agnostic module `CatalogueCore` in which will be established all business rules and policies

---
+++ CatalogueCore/CatalogueCore/CatalogueCore.h
@@ -0,0 +1,18 @@
+//
+//  CatalogueCore.h
+//  CatalogueCore
+//
+//  Created by Stanislav Dimitrov on 27.03.24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CatalogueCore.
+FOUNDATION_EXPORT double CatalogueCoreVersionNumber;
+
+//! Project version string for CatalogueCore.
+FOUNDATION_EXPORT const unsigned char CatalogueCoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CatalogueCore/PublicHeader.h>
+
+
---
+++ CatalogueCore/CatalogueCoreTests/CatalogueCoreTests.swift
@@ -0,0 +1,13 @@
+//
+//  CatalogueCoreTests.swift
+//  CatalogueCoreTests
+//
+//  Created by Stanislav Dimitrov on 27.03.24.
+//
+
+import XCTest
+import CatalogueCore
+
+final class CatalogueCoreTests: XCTestCase {
+
+}
