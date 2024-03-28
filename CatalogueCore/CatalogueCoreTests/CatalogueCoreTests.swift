0f95e6e7a7b9cbd04123d8ba5400b5b8d3ed4a5a
Stanislav Dimitrov <standimitroff@gmail.com>
2024-03-28 11:53:30 +0200

    `RemoteSportLoader` delivers `.noConnectionError` on `HTTPClient` error

--- CatalogueCore/CatalogueCoreTests/CatalogueCoreTests.swift
+++ CatalogueCore/CatalogueCoreTests/CatalogueCoreTests.swift
@@ -10,6 +10,8 @@ import CatalogueCore
 
 public typealias HTTPClientResult = Result<(Data, HTTPURLResponse), Error>
 
+public typealias LoadSportsResult = Result<[Sport], Error>
+
 protocol HTTPClient {
   func perform(request: URLRequest) async -> HTTPClientResult
 }
@@ -18,20 +20,27 @@ final class RemoteSportLoader {
   private let request: URLRequest
   private let client: HTTPClient
 
+  public enum Error: Swift.Error {
+    case noConnection
+    case invalidData
+  }
+
   public init(request: URLRequest, client: HTTPClient) {
     self.request = request
     self.client = client
   }
 
-  public func load() async {
-    let result = await client.perform(request: request)
+  public func load() async -> LoadSportsResult {
+    _ = await client.perform(request: request)
+
+    return .failure(Error.noConnection)
   }
 }
 
 final class CatalogueCoreTests: XCTestCase {
 
   func test_init_doesNotLoadDataFromURL() {
-    let (sut, client) = makeSUT()
+    let (_, client) = makeSUT()
 
     XCTAssertEqual(client.sentRequests, [])
   }
@@ -40,7 +49,7 @@ final class CatalogueCoreTests: XCTestCase {
     let request = URLRequest(url: URL(string: "https://test-url.com")!)
     let (sut, client) = makeSUT(request: request)
 
-    await sut.load()
+    let _ = await sut.load()
 
     XCTAssertEqual(client.sentRequests, [request])
   }
@@ -55,13 +64,22 @@ final class CatalogueCoreTests: XCTestCase {
     XCTAssertEqual(client.sentRequests, [request, request])
   }
 
-  private class HTTPClientSpy: HTTPClient {
 
-    private(set) var sentRequests = [URLRequest]()
+  // MARK: - Sad path
 
-    func perform(request: URLRequest) -> HTTPClientResult {
-      sentRequests.append(request)
-      return .success((Data(), HTTPURLResponse()))
+  func test_load_deliversErrorOnHTTPClientError() async {
+    let (sut, client) = makeSUT()
+
+    client.stub(result: nil, error: NSError(domain: "HTTPClient", code: 0))
+
+    let result = await sut.load()
+
+    switch result {
+    case let .failure(receivedError):
+      XCTAssertEqual(receivedError as! RemoteSportLoader.Error, RemoteSportLoader.Error.noConnection)
+
+    default:
+      XCTFail("Expected failure, got \(result) instead")
     }
   }
 
@@ -76,5 +94,29 @@ final class CatalogueCoreTests: XCTestCase {
     let sut = RemoteSportLoader(request: request, client: client)
 
     return (sut: sut, client: client)
+  }
+
+  private class HTTPClientSpy: HTTPClient {
+
+    struct Stub {
+      let result: (statusCode: Int, data: Data)?
+      let error: Error?
+    }
+
+    private(set) var sentRequests = [URLRequest]()
+    private var stub: Stub?
+
+    func stub(result: (statusCode: Int, data: Data)?, error: Error?) {
+      stub = Stub(result: result, error: error)
+    }
+
+    func perform(request: URLRequest) -> HTTPClientResult {
+      sentRequests.append(request)
+
+      if let error = stub?.error {
+        return .failure(error)
+      }
+      return .success((Data(), HTTPURLResponse(url: request.url!, statusCode: 200, httpVersion: nil, headerFields: nil)!))
+    }
   }
 }
