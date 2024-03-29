227b36beb8bf296d38e3e79a03325e6c128ce356
Stanislav Dimitrov <standimitroff@gmail.com>
2024-03-29 11:17:37 +0200

    Create iOS specific target `CatalogueiOS` with minimum deployment target iOS 16

---
+++ CatalogueCore/CatalogueiOS/CatalogueiOS.h
@@ -0,0 +1,18 @@
+//
+//  CatalogueiOS.h
+//  CatalogueiOS
+//
+//  Created by Stanislav Dimitrov on 29.03.24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CatalogueiOS.
+FOUNDATION_EXPORT double CatalogueiOSVersionNumber;
+
+//! Project version string for CatalogueiOS.
+FOUNDATION_EXPORT const unsigned char CatalogueiOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CatalogueiOS/PublicHeader.h>
+
+
