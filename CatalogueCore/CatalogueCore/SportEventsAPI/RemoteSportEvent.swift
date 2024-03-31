5cb2ca3d23b4a4fdb9a16b5cbb65eb8d9c23d074
Stanislav Dimitrov <standimitroff@gmail.com>
2024-03-31 16:44:08 +0300

    Create `RemoteSportEvent` structure which The BE response

--- CatalogueCore/CatalogueCore/SportEventsAPI/RemoteSportEvent.swift
+++ CatalogueCore/CatalogueCore/SportEventsAPI/RemoteSportEvent.swift
@@ -6,3 +6,26 @@
 //
 
 import Foundation
+
+struct RemoteSportEvent: Decodable {
+  let name: String
+  let date: Date
+  let primaryMarket: RemotePrimaryMarket
+}
+
+struct RemotePrimaryMarket: Decodable {
+  let name: String
+  let type: String
+  let runners: [RemoteRunner]
+}
+
+struct RemoteRunner: Decodable {
+  let name: String
+  let totalGoals: Int?
+  let odds: [RemoteOdd]
+}
+
+struct RemoteOdd: Decodable {
+  let numerator: Int
+  let denominator: Int
+}
