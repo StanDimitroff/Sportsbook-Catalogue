953697d11b48fc5fa08cbb545f1c23831394162d
Stanislav Dimitrov <standimitroff@gmail.com>
2024-04-01 10:04:49 +0300

    Configure `SportEventCell` in storyboard

--- CatalogueCore/CatalogueiOS/SportEventsUI/SportEventCell.swift
+++ CatalogueCore/CatalogueiOS/SportEventsUI/SportEventCell.swift
@@ -9,5 +9,15 @@ import UIKit
 
 public final class SportEventCell: UITableViewCell {
 
-  @IBOutlet public var nameLabel: UILabel!
+  @IBOutlet public var marketLabel: UILabel!
+  @IBOutlet public var matchLabel: UILabel!
+
+  @IBOutlet public var homeNumeratorLabel: UILabel!
+  @IBOutlet public var homeDenominatorLabel: UILabel!
+
+  @IBOutlet public var drawNumeratorLabel: UILabel!
+  @IBOutlet public var drawDenominatorLabel: UILabel!
+
+  @IBOutlet public var awayNumeratorLabel: UILabel!
+  @IBOutlet public var awayDenominatorLabel: UILabel!
 }
